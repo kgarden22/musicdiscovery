186e606a2ed8699e4febf2ee8d33aaca02cd7e69
kgarden22 <kgarden22@gmail.com>
2020-04-13 13:42:07 -0600

    commented out the index.js code as it is not going to work in time for presentation. This app will be finished but we dont have time today so lets just present what we have and explain our work

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-// authorize request
-const Spotify = require('spotify-web-api-node');
-const spotifyApi = new Spotify({
-  clientId: '39146e5690b2463780ac5fe7192bd1dd',
-  clientSecret: '6cedbf355b424807933f8995cf55549c',
-  redirectUri: 'http://musicselect://callback'
-});
+// // authorize request
+// const Spotify = require('spotify-web-api-node');
+// const spotifyApi = new Spotify({
+//   clientId: '39146e5690b2463780ac5fe7192bd1dd',
+//   clientSecret: '6cedbf355b424807933f8995cf55549c',
+//   redirectUri: 'http://musicselect://callback'
+// });
 
-// redirect to spotify authorization URL
-router.get('/login', (_, res) => {
-    const state = generateRandomString(16);
-    res.cookie(STATE_KEY, state);
-    res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
-  });
+// // redirect to spotify authorization URL
+// router.get('/login', (_, res) => {
+//     const state = generateRandomString(16);
+//     res.cookie(STATE_KEY, state);
+//     res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
+//   });
 
 
 
-// click function for drop down to select value from genre 
-function onClick(){
-    var genreSelect = $("#genre").change(function(event)
-    {console.log($(this).val())
-    var genreInput = $(this).val()
+// // click function for drop down to select value from genre 
+// function onClick(){
+//     var genreSelect = $("#genre").change(function(event)
+//     {console.log($(this).val())
+//     var genreInput = $(this).val()
     
     
     
     
-    })
-    console.log(genreSelect)
-}
-onClick()
+//     })
+//     console.log(genreSelect)
+// }
+// onClick()
 
